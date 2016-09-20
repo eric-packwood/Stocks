404a660fa9cd8762ba0e15a38de454bdb56eb64d
Eric Packwood <epackwood@gmail.com>
2016-09-20 18:38:53 -0400

    Data setup

--- Stocks.Data.DbSet/Properties/AssemblyInfo.cs
+++ Stocks.Data.DbSet/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Stocks.Data")]
+[assembly: AssemblyProduct("Stocks.Data.DbSet")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4b2308c0-2c0f-413a-936e-41203477fe7c")]
+[assembly: Guid("d01859ac-4a53-46e7-b865-eac0b4e729ca")]
